75a9b0139191202ab7c8998fd2efb39feb8a9fc6
DariaHaidabura <dariagaidabura99@gmail.com>
2021-09-24 20:23:16 +0300

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as List } from './List';
+export { default as AddList } from './AddList';
+export { default as printElements } from './test';
+export { default as Badge } from './Badge';
